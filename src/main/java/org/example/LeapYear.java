558bc7acf59fe5128dd6b5df6060a99a7a711f19
Maiwand Mohamand <maiwand@Maiwands-MacBook-Air.local>
2023-09-04 13:45:13 +0200

    initial commit

--- src/main/java/org/example/LeapYear.java
+++ src/main/java/org/example/LeapYear.java
@@ -1,4 +1,10 @@
 package org.example;
 
-public class LeapYear git{
-}
+    public class LeapYear {
+
+        public boolean isLeapYear(int year) {
+            boolean result;
+            if (year == 2023) return false;
+            return true;
+        }
+    }
